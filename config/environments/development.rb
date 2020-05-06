82c7bd95d0540031f5d426aa69c17fa78430bbb6
An Haru <haruan2394@gmail.com>
2020-05-06 21:34:04 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,13 +54,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => "haruan2394@gmail.com", #ご自身のgmailアドレス
-    :password => "Andou2394", #ご自身のgmailアドレスのパスワード
-    :authentication => 'login'
-  }
 end
